461dae733aadc18750c0efa18ff401eae8011485
Antoine <antoine.heitzmann2@gmail.com>
2019-03-19 12:04:57 +0100

    Creation partie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 
+use App\Entity\Partie;
 use App\Entity\User;
 use App\Entity\Ville;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class HomeController extends AbstractController
 {
@@ -29,5 +33,32 @@ class HomeController extends AbstractController
         $villes=$this->getDoctrine()->getRepository(Ville::class)->findAll();
 
         return $this->json(["villes"=>$villes]);
+    }
+
+    /**
+     * @Route("/create/partie",name="Partie.create")
+     * @Route("/vue/{route}", name="vue_index", requirements={"route"="^.+"})
+     * @param Request $request
+     * @param TokenGeneratorInterface $tgi
+     * @param ObjectManager $manager
+     */
+    public function createPartie(Request $request, TokenGeneratorInterface $tgi, ObjectManager $manager){
+        $token=$tgi->generateToken();
+        $pseudo=$request->get("pseudo");
+        $difficulte=$request->get("difficulte");
+        $ville=$request->get("ville");
+
+        $ville=$manager->getRepository(Ville::class)->findOneBy(['nom'=>$ville]);
+
+        $partie=new Partie();
+        $partie->setToken($token);
+        $partie->setPseudo($pseudo);
+        $partie->setDifficulte($difficulte);
+        $partie->setVille($ville);
+
+        $manager->persist($partie);
+        $manager->flush();
+
+        return $this->json(["partie"=>$partie]);
     }
 }
--- src/Entity/Ville.php
+++ src/Entity/Ville.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -25,6 +27,16 @@ class Ville
      * @ORM\Column(type="string", length=255)
      */
     private $pathPhoto;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Partie", mappedBy="ville")
+     */
+    private $parties;
+
+    public function __construct()
+    {
+        $this->parties = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -51,6 +63,37 @@ class Ville
     public function setPathPhoto(string $pathPhoto): self
     {
         $this->pathPhoto = $pathPhoto;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Partie[]
+     */
+    public function getParties(): Collection
+    {
+        return $this->parties;
+    }
+
+    public function addParty(Partie $party): self
+    {
+        if (!$this->parties->contains($party)) {
+            $this->parties[] = $party;
+            $party->setVille($this);
+        }
+
+        return $this;
+    }
+
+    public function removeParty(Partie $party): self
+    {
+        if ($this->parties->contains($party)) {
+            $this->parties->removeElement($party);
+            // set the owning side to null (unless already changed)
+            if ($party->getVille() === $this) {
+                $party->setVille(null);
+            }
+        }
 
         return $this;
     }
